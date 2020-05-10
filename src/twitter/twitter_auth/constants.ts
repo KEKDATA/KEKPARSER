6e6fbddd95156bf29e7674166f7782cc43e71d6a
TchernyavskyDaniil <tchernyavsky.daniil@yandex.ru>
2020-05-10 20:34:33 +0300

    remove magic selectors from auth

---
+++ src/twitter/twitter_auth/constants.ts
@@ -0,0 +1,12 @@
+export const AUTH_NAV_SELECTOR: string =
+  '.css-1dbjc4n.r-1awozwy.r-1pz39u2.r-18u37iz.r-16y2uox';
+export const LOG_IN_SELECTOR: string =
+  '.css-1dbjc4n.r-1awozwy.r-1pz39u2.r-18u37iz.r-16y2uox > div > a';
+
+export const INPUT_NAME_SELECTOR: string =
+  '[name="session[username_or_email]"].r-30o5oe';
+export const INPUT_PASSWORD_SELECTOR: string =
+  '[name="session[password]"].r-30o5oe';
+
+export const LOG_IN_BUTTON_SELECTOR: string =
+  '[data-testid="LoginForm_Login_Button"]';
--- src/twitter/twitter_auth/log_in.ts
+++ src/twitter/twitter_auth/log_in.ts
@@ -4,27 +4,26 @@ import { checkIsTwitterContentVisible } from '../lib/dom/visible_content_check';
 
 import { PROFILE_SELECTOR } from '../profile/constants/selectors';
 import { LOADER_SELECTOR } from '../constants/selectors';
+import {
+  AUTH_NAV_SELECTOR,
+  INPUT_NAME_SELECTOR,
+  INPUT_PASSWORD_SELECTOR,
+  LOG_IN_BUTTON_SELECTOR,
+  LOG_IN_SELECTOR,
+} from './constants';
 
 import { $webdriverPage } from '../model';
 
 export const logIn = async () => {
   const page = $webdriverPage.getState();
 
-  await page.waitForSelector(
-    '.css-1dbjc4n.r-1awozwy.r-1pz39u2.r-18u37iz.r-16y2uox',
-    { timeout: 1000 },
-  );
-  await page.click(
-    '.css-1dbjc4n.r-1awozwy.r-1pz39u2.r-18u37iz.r-16y2uox > div > a',
-  );
+  await page.waitForSelector(AUTH_NAV_SELECTOR, { timeout: 1000 });
+  await page.click(LOG_IN_SELECTOR);
 
-  await page.fill(
-    '[name="session[username_or_email]"].r-30o5oe',
-    AUTH_NICK_NAME,
-  );
-  await page.fill('[name="session[password]"].r-30o5oe', AUTH_PASSWORD);
+  await page.fill(INPUT_NAME_SELECTOR, AUTH_NICK_NAME);
+  await page.fill(INPUT_PASSWORD_SELECTOR, AUTH_PASSWORD);
 
-  await page.click('[data-testid="LoginForm_Login_Button"]');
+  await page.click(LOG_IN_BUTTON_SELECTOR);
 
   await page.waitForSelector(PROFILE_SELECTOR);
   await page.waitForFunction(checkIsTwitterContentVisible, LOADER_SELECTOR);
